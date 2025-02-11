fd750a42dcbf61bb9e1be5dcedc1a823d4bf4ab8
Shadow_CchiNnAa <138558778+Luanqmata@users.noreply.github.com>
2025-02-11 04:04:56 -0300

    FLAG PEQUENO GARFANHA

--- Decodificador.sh
+++ Decodificador.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# A string codificada em base64
 Flag_codificada_base_64="VHJ5SGFja01le1AzcXUzbjAtZzRmNG5oMHQwfQo="
 
-# Decodificando a string base64
 Flag_decodificada=$(echo "$Flag_codificada_base_64" | base64 --decode)
 
-# Imprimindo a flag decodificada
 echo "Flag decodificada: $Flag_decodificada"
